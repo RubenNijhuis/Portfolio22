93adc53d4e69c6fce90218647d3bfc346806ebb7
Ruben Nijhuis <contact@rubennijhuis.com>
2022-01-19 21:53:19 +0100

    styling change on nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,31 +4,27 @@ import React from "react";
 import { Link } from "gatsby";
 
 // Styling
-import "./styling.scss"
+import "./styling.scss";
 
 const Nav = () => {
+  const className = "nav";
+
   return (
-    <nav className="nav">
-      <Link to="/" className="home">
-        RN
-      </Link>
-      <ul className="links">
-        <li>
-          <Link className="links__link" to="/projects">
-            Projects
+    <nav className={className}>
+      <div className="links">
+        <div className="col--home">
+          <Link to="/" className="home">
+            RN
           </Link>
-        </li>
-        <li>
-          <Link className="links__link" to="/journal">
-            Journal
-          </Link>
-        </li>
-        <li>
-          <Link className="links__link" to="/about">
-            About
-          </Link>
-        </li>
-      </ul>
+        </div>
+        <div className="col--main">
+          <Link to="/projects">Projects</Link>
+          <Link to="/journal">Journal</Link>
+        </div>
+        <div className="col--secondary">
+          <Link to="/about">About</Link>
+        </div>
+      </div>
     </nav>
   );
 };
