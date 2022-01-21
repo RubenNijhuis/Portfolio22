3b97880a49919c2478b81cce9b0c2e966876f016
Ruben Nijhuis <contact@rubennijhuis.com>
2022-01-21 10:46:22 +0100

    🎉 feat: possible to exclude pages through main-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,44 @@
 const site_config = require("./site-config");
 require("dotenv").config({
-    path: `.env`,
+  path: `.env`,
 });
 
 module.exports = {
-    siteMetadata: { ...site_config },
-    plugins: [
-        "gatsby-plugin-react-helmet",
-        "gatsby-plugin-sitemap",
-        "gatsby-plugin-image",
-        `gatsby-plugin-sass`,
-        "gatsby-plugin-sharp",
-        "gatsby-transformer-sharp",
-        `gatsby-plugin-glslify`,
-        {
-            resolve: `gatsby-source-contentful`,
-            options: {
-                spaceId: process.env.CONTENTFUL_SPACE_ID,
-                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-            },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                name: "assets",
-                path: "./src/assets/",
-            },
-            __key: "images",
-        },
-        // {
-        //   resolve: "gatsby-plugin-google-analytics",
-        //   options: {
-        //     trackingId: "",
-        //   },
-        // },
-    ],
+  siteMetadata: { ...site_config.siteMetadata },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-image",
+    `gatsby-plugin-sass`,
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    `gatsby-plugin-glslify`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "assets",
+        path: "./src/assets/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-plugin-exclude",
+      options: {
+        paths: site_config.excludePaths,
+      },
+    },
+    // {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackiangId: "",
+    //   },
+    // },
+  ],
 };
--- site-config.js
+++ site-config.js
@@ -1,21 +1,31 @@
 const path = require("path");
 
 module.exports = {
+  siteMetaData: {
+    // Site descriptions
     siteUrl: "https://rubennijhuis.com",
     siteTitle: "Ruben Nijhuis | Design && Development",
     siteTitleShort: "Ruben Nijhuis",
     siteDescription:
-        "Ruben Nijhuis is a Designer and Developer who creates Unique Digital Experiences using Creative Coding and Interactive Design",
+      "Ruben Nijhuis is a Designer and Developer who creates Unique Digital Experiences using Creative Coding and Interactive Design",
+
+    // Site colors
     themeColor: "#e1e1e1",
-    themeColor: `#1e1e1e`,
     backgroundColor: `#1e1e1e`,
+
+    // Favicon
     logo: path.resolve(__dirname, "src/images/icons/favicon.png"),
+
+    // Social links
     social: {
-        twitter: "nijhuis_ruben",
-        instagram: "ruben__nijhuis",
-        fbAppId: "aa",
-        dribble: "SoWhatsUp",
-        github: "rubennijhuis",
-        codepen: "rubennijhuis",
+      twitter: "nijhuis_ruben",
+      instagram: "ruben__nijhuis",
+      fbAppId: "aa",
+      dribble: "SoWhatsUp",
+      github: "rubennijhuis",
+      codepen: "rubennijhuis",
     },
+  },
+  excludePaths: ["/about/"],
+  googleAnalyticsTrackingId: "",
 };
