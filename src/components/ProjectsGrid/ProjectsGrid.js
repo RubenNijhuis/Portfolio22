992043d29740bc2b10a2dd9a9829ecba9e5c6653
Ruben Nijhuis <contact@rubennijhuis.com>
2022-05-07 11:04:56 +0200

    fix: swapped href in projects grid

--- src/components/ProjectsGrid/ProjectsGrid.js
+++ src/components/ProjectsGrid/ProjectsGrid.js
@@ -2,13 +2,7 @@ import React from "react";
 import propTypes from "prop-types";
 
 // Utils
-import { transform_project } from "utils/datatype-transformers";
-import {
-    swap_array_elements,
-    useCurrentWidth,
-    isBrowser
-} from "utils/helper-functions";
-import { sizes } from "utils/constants";
+import { isBrowser } from "utils/helper-functions";
 import { limits } from "constants/limits";
 
 // Components
@@ -20,16 +14,11 @@ import "./style.scss";
 
 const ProjectsGrid = ({ projects, animate = true, limit = false }) => {
     const amount_projects = projects.length;
-    const width = useCurrentWidth();
     const className = "projects-grid";
 
-    // Swap projects before render (fixes items rebounce)
-    let projects_parsed =
-        width > sizes.small ? swap_array_elements(projects) : projects;
-
     // Slice array if bigger than limit
-    if (limit === true && limits.amount_projects < projects_parsed.length)
-        projects_parsed = projects_parsed.slice(0, limits.amount_projects);
+    if (limit === true && limits.amount_projects < projects.length)
+        projects = projects.slice(0, limits.amount_projects);
 
     let isHomePage = false;
     if (isBrowser) {
@@ -41,24 +30,29 @@ const ProjectsGrid = ({ projects, animate = true, limit = false }) => {
         <section className={className}>
             <h2 className="heading">Projects</h2>
             <div className="grid">
-                {width !== undefined &&
-                    projects_parsed.map((project, index) => {
-                        const { name, tags, description, showcase_img, background_color } = project;
-                        const parsed_tags = tags.split(" | ");
-                        return (
-                            <Project
-                                offset={isHomePage}
-                                animate={animate}
-                                key={index}
-                                name={name}
-                                description={description}
-                                img={showcase_img}
-                                tags={parsed_tags}
-                                background={background_color}
-                                count={index}
-                            />
-                        );
-                    })}
+                {projects.map((project, index) => {
+                    const {
+                        name,
+                        tags,
+                        description,
+                        showcase_img,
+                        background_color
+                    } = project;
+                    const parsed_tags = tags.split(" | ");
+                    return (
+                        <Project
+                            offset={isHomePage}
+                            animate={animate}
+                            key={index}
+                            name={name}
+                            description={description}
+                            img={showcase_img}
+                            tags={parsed_tags}
+                            background={background_color}
+                            count={index}
+                        />
+                    );
+                })}
             </div>
             {limit ? (
                 <SeeMore
